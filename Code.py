17796f20ea93a1aa89394eadac6d68e943754acc
Kp1taine <lea.chabbal@gmail.com>
2024-04-10 15:47:33 +0200

    import des donnees excel dans le code et astres dans la base de donnees

--- Code.py
+++ Code.py
@@ -32,19 +32,17 @@ def input(key):
 
 
 
-sun = CelestialBodies(position=(0,0,0),scale=50,texture='assets/8k_sun')
+sun=CelestialBodies(position=(sheet['B2'].value,sheet['C2'].value,sheet['D2'].value),orbitSpeed =(sheet['E2'].value, sheet['F2'].value ,sheet['G2'].value),rotation=(sheet['H2'].value,sheet['I2'].value,sheet['J2'].value),orbitRadius=sheet['K2'].value, rotationSpeed=[0,1.99,0],scale=sheet['O2'].value,texture=sheet['P2'].value)
 sun.sun=True
 
-#mercury=CelestialBodies(position=(60,0,0), scale=0.35035,texture='assets/mercury')
-#venus=CelestialBodies(70,0,0,0.8609,'assets/venus1')
-earth=CelestialBodies(position=(sheet['B5'].value,sheet['C5'].value,sheet['D5'].value),orbitSpeed =(360/365, 0 ,0),rotation=(23.45,0,0),orbitRadius=80, rotationSpeed=[0,-360/24,0],scale=0.9159,texture='assets/earth')
-#earth=CelestialBodies(position=(80,0,0),orbitSpeed =(360/365, 0 ,0),rotation=(23.45,0,0),orbitRadius=80, rotationSpeed=[0,-360/24,0],scale=0.9159,texture='assets/earth')
-
-#mars=CelestialBodies(90,0,0,0.4876,'assets/mars')
-#jupiter=CelestialBodies(110,0,0,10.2667,'assets/jupiter')
-#saturn=CelestialBodies(140,0,0,3.5562,'assets/saturn')
-#uranus=CelestialBodies(180,0,0,3.6704,'assets/uranus')
-#neptune = CelestialBodies(210,0,0,3.5562,'assets/neptune')
+mercury=CelestialBodies(position=(sheet['B3'].value,sheet['C3'].value,sheet['D3'].value),orbitSpeed =(sheet['E3'].value, sheet['F3'].value ,sheet['G3'].value),rotation=(sheet['H3'].value,sheet['I3'].value,sheet['J3'].value),orbitRadius=sheet['K3'].value, rotationSpeed=[0,10,89,0],scale=sheet['O3'].value,texture=sheet['P3'].value)
+venus=CelestialBodies(position=(sheet['B4'].value,sheet['C4'].value,sheet['D4'].value),orbitSpeed =(sheet['E4'].value, sheet['F4'].value ,sheet['G4'].value),rotation=(sheet['H4'].value,sheet['I4'].value,sheet['J4'].value),orbitRadius=sheet['K4'].value, rotationSpeed=[0,6.52,0],scale=sheet['O4'].value,texture=sheet['P4'].value)
+earth=CelestialBodies(position=(sheet['B5'].value,sheet['C5'].value,sheet['D5'].value),orbitSpeed =(sheet['E5'].value, sheet['F5'].value ,sheet['G5'].value),rotation=(sheet['H5'].value,sheet['I5'].value,sheet['J5'].value),orbitRadius=sheet['K5'].value, rotationSpeed=[0,17000,0],scale=sheet['O5'].value,texture=sheet['P5'].value)
+mars=CelestialBodies(position=(sheet['B6'].value,sheet['C6'].value,sheet['D6'].value),orbitSpeed =(sheet['E6'].value, sheet['F6'].value ,sheet['G6'].value),rotation=(sheet['H6'].value,sheet['I6'].value,sheet['J6'].value),orbitRadius=sheet['K6'].value, rotationSpeed=[0,-360/24,0],scale=sheet['O6'].value,texture=sheet['P6'].value)
+jupiter=CelestialBodies(position=(sheet['B7'].value,sheet['C7'].value,sheet['D7'].value),orbitSpeed =(sheet['E7'].value, sheet['F7'].value ,sheet['G7'].value),rotation=(sheet['H7'].value,sheet['I7'].value,sheet['J7'].value),orbitRadius=sheet['K7'].value, rotationSpeed=[0,-360/24,0],scale=sheet['O7'].value,texture=sheet['P7'].value)
+saturn=CelestialBodies(position=(sheet['B8'].value,sheet['C8'].value,sheet['D8'].value),orbitSpeed =(sheet['E8'].value, sheet['F8'].value ,sheet['G8'].value),rotation=(sheet['H8'].value,sheet['I8'].value,sheet['J8'].value),orbitRadius=sheet['K8'].value, rotationSpeed=[0,-360/24,0],scale=sheet['O8'].value,texture=sheet['P8'].value)
+uranus=CelestialBodies(position=(sheet['B9'].value,sheet['C9'].value,sheet['D9'].value),orbitSpeed =(sheet['E9'].value, sheet['F9'].value ,sheet['G9'].value),rotation=(sheet['H9'].value,sheet['I9'].value,sheet['J9'].value),orbitRadius=sheet['K9'].value, rotationSpeed=[0,-360/24,0],scale=sheet['O9'].value,texture=sheet['P9'].value)
+neptune=CelestialBodies(position=(sheet['B10'].value,sheet['C10'].value,sheet['D10'].value),orbitSpeed =(sheet['E10'].value, sheet['F10'].value ,sheet['G10'].value),rotation=(sheet['H10'].value,sheet['I10'].value,sheet['J10'].value),orbitRadius=sheet['K10'].value, rotationSpeed=[0,-360/24,0],scale=sheet['O10'].value,texture=sheet['P10'].value)
 
 
 
